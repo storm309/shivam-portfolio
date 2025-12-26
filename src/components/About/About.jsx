6a1d07d370741cb1be4c6585b730f5ec2a0a10d8
Shivam Pandey <shivamkumarp447@gmail.com>
2025-12-26 16:56:55 +0530

    Update About section content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/17S_baFwHFwEnXTgBVaimJI7tRDUS2fA0/view?usp=sharing"
+            href="https://drive.google.com/file/d/1eSyR36aMtjwDqsbVN-hUhKMqNX44INNn/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
